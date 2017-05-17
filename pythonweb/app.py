1c6831cc6096869450b9ceed7ecbc445663fda1f
moneyDboat <moneyboat@foxmail.com>
2017-05-17 22:22:50 +0800

    add pythonweb

---
+++ pythonweb/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+	return render_template('home.html')
+
+@app.route('/signin', methods=['GET'])
+def signin_form():
+	return render_template('form.html')
+
+@app.route('/signin', methods=['POST'])
+def signin():
+	username = request.form['username']
+	password = request.form['password']
+	# 需要从request对象读取表单内容：
+	if username=='admin' and password=='111':
+		return render_template('signin-ok.html', username=username)
+	return render_template('form.html', message='Bad username or password', username=username)
+
+if __name__ == '__main__':
+	app.run()
