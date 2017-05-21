51a474d68469fba91a218076b7ca5539183d96b6
moneyDboat <moneyboat@foxmail.com>
2017-05-21 23:38:44 +0800

    python web backage

--- pythonweb/hello.py
+++
@@ -1,5 +0,0 @@
-def application(environ, start_response):
-	start_response('200 OK', [('Content-Type', 'text/html')])
-	body =  '<h1>Hello, %s!</h1>' % (environ['PATH_INFO'][1:] 
-		or 'web')
-	return [body.encode('utf-8')]
